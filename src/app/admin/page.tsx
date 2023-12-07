820c84e30cbddb592fe833a6fc4ddbe65cbf2eec
Виталий <brynzav2@gmail.com>
2023-12-07 13:31:45 +0200

    Начало работы в Next

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  style  from './page.module.css'
+
+
+const Admin = () => {
+  return (
+    <div className= {style.admin}>Admin</div>
+  )
+}
+
+export default Admin
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+
+import  type { PropsWithChildren } from 'react'
+
+function Dasbordlayout ({children}: PropsWithChildren<unknown>) {
+  return (
+    <div>
+        <h1>Dasbordlayout</h1>
+        {children}
+    </div>
+  )
+}
+
+export default Dasbordlayout
+
